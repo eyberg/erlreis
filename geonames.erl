8d7c10fd8f3fa20bfed466b21d5996db1af98114
cyn0n <cyn0n@spicetrader.(none)>
2008-08-28 21:54:47 -0500

    wow! two hole functions -- woop de fucking doo; next stop: spec alley

--- geonames.erl
+++ geonames.erl
@@ -1,19 +1,36 @@
 -module(geonames).
 -export([city_from_zip/1]).
+-export([cities_by_bound/4]).
+
 -include_lib("xmerl/include/xmerl.hrl").
 
--define(BASE_URL, 
-      "http://ws.geonames.org/" ++
-      "postalCodeSearch?" ++
-      "maxRows=1&postalcode=").
+-define(GEO_URL, 
+      "http://ws.geonames.org/").
 
-city_from_zip(ISBN) ->
-  URL = amazon_url_for(ISBN),
+%% find city, state from zip code
+%% takes: zip code
+%% returns: city, state
+city_from_zip(ZIP) ->
+  PARAMS = "&postalcode=" ++ ZIP,
+  URL = geonames_url("postalCodeSearch", PARAMS, "1"),
   { ok, {_Status, _Headers, Body }} = http:request(URL),
   { Xml, _Rest } = xmerl_scan:string(Body),
   [ #xmlText{value=City} ]  = xmerl_xpath:string("//name/text()", Xml),
   [ #xmlText{value=State} ] = xmerl_xpath:string("//adminCode1/text()", Xml),
   { State, City }.
 
-amazon_url_for(ZIP) ->
-  ?BASE_URL ++ ZIP.
+%% return list of cities in bounding box of N,E,S,W
+%% takes: n, s, e, w coordinates
+%% returns: city
+cities_by_bound(NORTH, SOUTH, EAST, WEST) ->
+  PARAMS = "&north=" ++ NORTH ++ "&south=" ++ SOUTH ++ "&east=" ++ 
+            EAST ++ "&west=" ++ WEST ++ "",
+  URL = geonames_url("cities", PARAMS, "1"),
+  { ok, {_Status, _Headers, Body }} = http:request(URL),
+  { Xml, _Rest } = xmerl_scan:string(Body),
+  [ #xmlText{value=City} ]  = xmerl_xpath:string("//name/text()", Xml),
+  { City }.
+
+%% build url for the search
+geonames_url(SVC, PARAMS, COUNT) ->
+  ?GEO_URL ++ SVC ++ "?maxRows=" ++ COUNT ++ PARAMS.
