fe665cee6fffed0e61d82e4cffdbfdea75a9614e
cyn0n <cyn0n@spicetrader.(none)>
2008-08-28 22:40:12 -0500

    adding in countrycode locator && neighboring countries locator

--- geonames.erl
+++ geonames.erl
@@ -1,6 +1,8 @@
 -module(geonames).
 -export([city_from_zip/1]).
 -export([cities_by_bound/4]).
+-export([neighboring_countries_for/1]).
+-export([countrycode_for/2]).
 
 -include_lib("xmerl/include/xmerl.hrl").
 
@@ -31,6 +33,30 @@ cities_by_bound(NORTH, SOUTH, EAST, WEST) ->
   [ #xmlText{value=City} ]  = xmerl_xpath:string("//name/text()", Xml),
   { City }.
 
+%% grab a list for all the surrounding neighbors of a country
+%% takes: a geonameid
+%% returns: list of countries surrounding that country
+%% FUTURE: make it accessible by name of the country
+%% NOTE:: I locked the xpath quuery down to the first element
+%% because this doesn't have a geonamedid count limit
+neighboring_countries_for(COUNTRY) ->
+  PARAMS = "&geonameId=" ++ COUNTRY,
+  URL = geonames_url("neighbours", PARAMS, "1"),
+  { ok, {_Status, _Headers, Body }} = http:request(URL),
+  { Xml, _Rest } = xmerl_scan:string(Body),
+  [ #xmlText{value=Countries} ] = xmerl_xpath:string("//name/text()[1]", Xml),
+  { Countries }.
+
+%% grab the country code for any longitude, latitutde combo
+%% takes: latitude, longitude
+%% returns: country code
+countrycode_for(LONGITUDE, LATITUDE) ->
+  PARAMS = "&lat=" ++ LATITUDE ++ "&lng=" ++ LONGITUDE,
+  URL = geonames_url("countryCode", PARAMS, "1"),
+  { ok, {_Status, _Headers, Body }} = http:request(URL),
+  {_, CountryCode, _} = regexp:sub(Body, "\r\n", ""),
+  { CountryCode }.
+  
 %% build url for the search
 geonames_url(SVC, PARAMS, COUNT) ->
   ?GEO_URL ++ SVC ++ "?maxRows=" ++ COUNT ++ PARAMS.
